c98ebf2460f13370393e66fae47ceab1a72e99fe
Orion Campos <oriongmc@hotmail.com>
2022-08-30 00:24:17 -0300

    ifelse

---
+++ Basics/EvenOdd/EvenOdd.py
@@ -0,0 +1,7 @@
+number = int(input("Which number do you want to check? "))
+if number % 2 != 0:
+    print("This is an odd number.")
+elif number == 0:
+    print("This number is zero.")
+else:
+    print("This is an even number.")
